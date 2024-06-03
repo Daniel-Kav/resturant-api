ba365b318fe45f39af19f78c18e8780eeb4b940e
daniel-kav <dankavatha01@gmail.com>
2024-06-03 16:08:24 +0300

    creating more relations

--- src/drizzle/schema.ts
+++ src/drizzle/schema.ts
@@ -1,4 +1,6 @@
-import { pgTable, serial, text, varchar, integer } from "drizzle-orm/pg-core";
+// import { pgTable, serial, text, varchar, integer } from "drizzle-orm/pg-core";
+import { pgTable, serial, text, varchar, integer, boolean, decimal, timestamp } from 'drizzle-orm/pg-core';
+
 
 //user table
 export const UsersTable = pgTable("users", {
@@ -13,6 +15,18 @@ export const ProfilesTable = pgTable("profiles", {
     id: serial("id").primaryKey(),
     bio: varchar("bio", { length: 256 }),
     userId: integer("user_id").notNull().references(() => UsersTable.id, { onDelete: "cascade" }), //fk ref id in users table
+});
+
+export const AddressTable = pgTable('address', {
+    id: serial('id').primaryKey(),
+    street_address_1: varchar('street_address_1', {length: 255}),
+    street_address_2: varchar('street_address_2', {length: 255}),
+    zip_code: varchar('zip_code', {length: 50}),
+    delivery_instructions: text('delivery_instructions'),
+    user_id: integer('user_id'),
+    city_id: integer('city_id'),
+    created_at: timestamp('created_at').defaultNow(),
+    updated_at: timestamp('updated_at').defaultNow()
 });
 
 
